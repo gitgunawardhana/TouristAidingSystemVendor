a2f78776dd3ad7b9ddaf8db441f3a25d65d718cb
Sanidu-Udawatta <saniduhasanka@gmail.com>
2022-07-24 03:31:45 +0530

    added provide

---
+++ src/Component/Main/Provide/Provide.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Provide.css"
+import provide from "../../../Assets/Main/Provide.png"
+
+function Provide() {
+    return (
+        <div className="Provide-main-div">
+            <img src={provide} alt="provide-img" className="provide-img"/>
+            <div className="sub-img-div">
+
+
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Provide;
