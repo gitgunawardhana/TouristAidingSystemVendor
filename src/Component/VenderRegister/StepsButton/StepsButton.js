da68c2dbece1284ca826b75aad1dea53a7066f86
Sanidu-Udawatta <saniduhasanka@gmail.com>
2022-07-25 01:58:32 +0530

    updated file

--- src/Component/VenderRegister/StepsButton/StepsButton.js
+++ src/Component/VenderRegister/StepsButton/StepsButton.js
@@ -1,36 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./StepsButton.css";
 
 function StepsButton() {
   const location = useLocation();
   const currentPath = location.pathname;
+
   return (
     <div>
       <div className="container">
         <div className="steps-nav">
           <nav className="nav nav-borders nav-border  nav-fill  border w-75 p-3  ">
+
             <Link
-              to="/basicdetails"
-              className={
-                currentPath.includes("basicdetails")
-                  ? "nav-link  ms-0 nav-bar-link active"
-                  : "nav-link  ms-0 nav-bar-link"
-              }
+                to="/basicDetails" onClick={e => e.preventDefault()}
+                className={
+                  currentPath.includes("basicDetails" )
+                      ? "nav-link  ms-0 nav-bar-link active"
+                      : "nav-link  ms-0 nav-bar-link"
+                }
             >
               Basics details
             </Link>
             <Link
-              to="/location"
+              to="/location" onClick={e => e.preventDefault()}
               className={
-                currentPath.includes("location")
+                currentPath.includes("location" )
                   ? "nav-link  ms-0 nav-bar-link active"
                   : "nav-link  ms-0 nav-bar-link"
               }
             >
               Location
             </Link>
-            <Link
+            <Link onClick={e => e.preventDefault()}
               to="/houserules"
               className={
                 currentPath.includes("houserules")
@@ -40,7 +42,7 @@ function StepsButton() {
             >
               House Rules
             </Link>
-            <Link
+            <Link onClick={e => e.preventDefault()}
               to="/facilities"
               className={
                 currentPath.includes("facilities")
@@ -50,12 +52,12 @@ function StepsButton() {
             >
               Facilities
             </Link>
-            <Link
+            <Link onClick={e => e.preventDefault()}
               to="/photos"
               className={
                 currentPath.includes("photos")
                   ? "nav-link  ms-0 nav-bar-link active"
-                  : "nav-link  ms-0 nav-bar-link"
+                  : "nav-link  ms-0 nav-bar-link cursor-na "
               }
             >
               Photos
