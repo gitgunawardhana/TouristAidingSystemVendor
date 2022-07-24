4a79d843188f524b16e5e6562f20b9ef798c47b2
ishan <t.gunawardana864@gmail.com>
2022-07-24 18:20:28 +0530

    Changed path location

--- src/View/AccommodationRegistrationBasicDetails/AccommodationRegistrationBasicDetails.js
+++ src/View/AccommodationRegistrationBasicDetails/AccommodationRegistrationBasicDetails.js
@@ -3,8 +3,8 @@ import ShoppingBags from "../../Assets/BasicDetails/ShoppingBags.png";
 import Footer from "../../Component/Footer/Footer";
 import RegPageNavbar from "../../Component/RegistrationPageNavbar/RegPageNavbar";
 import FormCompo from "../../Component/VenderRegister/BasicDetails/FormCompo/FormCompo";
-import StepsButton from "../../Component/VenderRegister/BasicDetails/StepsButton/StepsButton";
 import DescriptionCompo from "../../Component/VenderRegister/DescriptionCompo/DescriptionCompo";
+import StepsButton from "../../Component/VenderRegister/StepsButton/StepsButton";
 
 function AccommodationRegistrationBasicDetails() {
   return (
