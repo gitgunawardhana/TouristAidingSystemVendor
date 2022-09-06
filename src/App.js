bfcefdda8047e0bf89f249d9009ce556fbf4e802
Sanidu-Udawatta <saniduhasanka@gmail.com>
2022-09-07 01:10:10 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import VehicleRegInfo from "../src/View/VehicleRegistrationInfo/VehicleRegistrationInfo.js";
@@ -16,76 +16,92 @@ import CreateAccount from "./View/CreateAccount/CreateAccount";
 import Main from "./View/Main/Main";
 import SignIn from "./View/SignIn/SignIn";
 import DashboardMain from "./View/VendorDashboard/Dashboard";
-import VendorDashboardAccomodationView from "./View/VendorDashboard/vendorDashboardAccomodationView";
-// import VehicleRegOwnerInfo from '../src/View/VehicleRegistrationOwnerInfo/VehicleRegOwnerInfo.js'
+import VendorDashboardAccomodationView from "./View/VendorDashboard/vendorDashboardVehicleBookingsView";
 import VehicleRegDriverInfo from "../src/View/VehicleRegDriverDetails/VehicleRegDriverDetails.js";
 import AddRoom from "./Component/VendorDashboard/AddRoom/AddRoom";
-import AccommodationRegistrationLocation from "./View/AccommodationRegistrationLocation/AccommodationRegistrationLocation";
+import AccommodationRegistrationLocation
+    from "./View/AccommodationRegistrationLocation/AccommodationRegistrationLocation";
 import VendorType from "./View/VenderType/VenderType";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/vendorDashBoardMain" element={<DashboardMain />} />
-          <Route
-            path="/vendorDashboardAccomodationView"
-            element={<VendorDashboardAccomodationView />}
-          />
-          <Route
-            path="//configurations/add-accommodation"
-            element={<AddRoom />}
-          />
-          <Route path="/createAccount" element={<CreateAccount />} />
-          <Route path="/emailVerification" element={<EmailVerification />} />
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/vendor-type" element={<VendorType />} />
-          <Route
-            path="/basicDetails"
-            element={<AccommodationRegistrationBasicDetails />}
-          />
-          <Route path="/vender-vehicle-reg-info" element={<VehicleRegInfo />} />
-          <Route
-            path="/vender-vehicle-reg-vehicle"
-            element={<VehicleRegVehicle />}
-          />
-          <Route
-            path="/vender-vehicle-reg-photo"
-            element={<VehicleRegPhotos />}
-          />
-          {/* <Route path="/vender-vehicle-owner-info" element={< VehicleRegOwnerInfo />} /> */}
-          <Route
-            path="/vender-vehicle-driver-info"
-            element={<VehicleRegDriverInfo />}
-          />
-          <Route
-            path="/basicDetails"
-            element={<AccommodationRegistrationBasicDetails />}
-          />
-          <Route path="/photos" element={<AccommodationRegistrationPhotos />} />
-          <Route
-            path="/facilities"
-            element={<AccommodationRegistrationFacilities />}
-          />
-          <Route
-            path="/location"
-            element={<AccommodationRegistrationLocation />}
-          />
-          <Route
-            path="/facilities"
-            element={<AccommodationRegistrationFacilities />}
-          />
-          <Route
-            path="/houseRules"
-            element={<AccommodationRegistrationHouseRules />}
-          />
-          <Route path="/regPageNavbar" element={<RegPageNavbar />} />
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Main/>}/>
+                    <Route path="/vendorDashBoardMain" element={<DashboardMain/>}/>
+
+                    <Route
+                        path="/vendorDashboardAccomodationView"
+                        element={<VendorDashboardAccomodationView/>}
+                    />
+
+                    <Route
+                        path="//configurations/add-accommodation"
+                        element={<AddRoom/>}
+                    />
+
+                    <Route path="/createAccount" element={<CreateAccount/>}/>
+                    <Route path="/emailVerification" element={<EmailVerification/>}/>
+                    <Route path="/signIn" element={<SignIn/>}/>
+                    <Route path="/vendor-type" element={<VendorType/>}/>
+
+                    <Route
+                        path="/basicDetails"
+                        element={<AccommodationRegistrationBasicDetails/>}
+                    />
+
+                    <Route path="/vender-vehicle-reg-info" element={<VehicleRegInfo/>}/>
+
+                    <Route
+                        path="/vender-vehicle-reg-vehicle"
+                        element={<VehicleRegVehicle/>}
+                    />
+
+                    <Route
+                        path="/vender-vehicle-reg-photo"
+                        element={<VehicleRegPhotos/>}
+                    />
+
+                    {/* <Route path="/vender-vehicle-owner-info" element={< VehicleRegOwnerInfo />} /> */}
+                    <Route
+                        path="/vender-vehicle-driver-info"
+                        element={<VehicleRegDriverInfo/>}
+                    />
+
+                    <Route
+                        path="/basicDetails"
+                        element={<AccommodationRegistrationBasicDetails/>}
+                    />
+
+                    <Route path="/photos" element={<AccommodationRegistrationPhotos/>}/>
+
+                    <Route
+                        path="/facilities"
+                        element={<AccommodationRegistrationFacilities/>}
+                    />
+
+                    <Route
+                        path="/location"
+                        element={<AccommodationRegistrationLocation/>}
+                    />
+
+                    <Route
+                        path="/facilities"
+                        element={<AccommodationRegistrationFacilities/>}
+                    />
+
+                    <Route
+                        path="/houseRules"
+                        element={<AccommodationRegistrationHouseRules/>}
+                    />
+
+                    <Route path="/regPageNavbar" element={<RegPageNavbar/>}/>
+
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
--- src/Component/CreateAccount/FirstPage/CreateAccountCom.js
+++ src/Component/CreateAccount/FirstPage/CreateAccountCom.js
@@ -3,117 +3,205 @@ import "./CreateAccountCom.css"
 import trainpic from "../../../Assets/CreateAccount/BadullaTrain.png";
 import PhoneInput from "react-phone-number-input";
 import 'react-phone-number-input/style.css'
-import {Link} from "react-router-dom";
+import {createSearchParams, Link} from "react-router-dom";
+import Swal from "sweetalert2";
+import axios from "axios";
+import {useNavigate} from "react-router";
 
-function CreateAccountCom() {
-    const [value, setValue] = useState()
-    return (
-        <div className="create-account-main-div">
-            <section className="h-100 bg-dark">
-                <div className="container py-5 h-100 ">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col ">
-                            <div className="card card-registration my-4 shadow-lg p-3 mb-5 bg-white rounded">
-                                <div className="row g-0">
-                                    <div className="col-xl-6 d-none d-xl-block">
-                                        <img src={trainpic} alt="train" className="train-img img-fluid"/>
-                                    </div>
-                                    <div className="col-xl-6">
-                                        <Link to="/">
-                                            <button type="button" className="btn-close" aria-label="Close"></button>
-                                        </Link>
+ function CreateAccountCom() {
+        const [firstName, setFirstName] = useState("");
+        const [lastName, setLastName] = useState("");
+        const [mobile, setMobile] = useState("");
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+        const navigate = useNavigate();
 
-                                        <div className="card-body p-md-5 text-black">
-                                            <div className="logo-name shadow p-3 mb-5 bg-white rounded">
-                                                <p className="sub-logo-name"> RodeSign</p>
-                                            </div>
+        const handleFirstNameChange = event => {
+            setFirstName(event.target.value);
+        }
+
+        const handleLastNameChange = event => {
+            setLastName(event.target.value);
+        }
+
+        const handleMobileChange = event => {
+            setMobile(event.target.value);
+        }
+
+        const handleEmailChange = event => {
+            setEmail(event.target.value);
+        }
+
+        const handlePasswordChange = event => {
+            setPassword(event.target.value)
+        }
+        const handleSubmit = event => {
+
+            const Details = {
+                firstName: firstName,
+                lastName: lastName,
+                mobile: mobile,
+                email: email,
+                password: password
+            }
 
 
-                                            <div className="acc-des text-uppercase text-center mb-5">
-                                                Let’s Create Your Account
-                                            </div>
+            axios.post("http://localhost:8080/vendor/register", Details)
+                .then(res => {
+                    if (res.data.success) {
+                        // window.location = "/emailVerification"
+                        // navigate("/emailVerification"))
+                        navigate({
+                            pathname: "/emailVerification",
+                            search: createSearchParams({
+                                email: email
+                            }).toString()
+                        });
 
 
-                                            <div className="row">
-                                                <div className="col-md-6 mb-4">
-                                                    <div className="form-outline">
-                                                        <label className="form-label" htmlFor="form3Example1m">First
-                                                            name</label>
-                                                        <input type="text" id="form3Example1m"
-                                                               className="form-control form-control-lg" placeholder="First
-                                                            name"/>
+                    } else {
+                        Swal.fire(
+                            res.data.message,
+                            'error'
+                        ).then(r => {
+                        })
+                    }
 
+                })
+                .catch(err => {
+                    Swal.fire(
+                        'Failed',
+                        'Something went wrong',
+                        'error'
+                    ).then(r => {
+                    })
+                })
+        }
+
+        const [value, setValue] = useState()
+        return (
+            <div className="create-account-main-div">
+                <section className="h-100 bg-dark">
+                    <div className="container py-5 h-100 ">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col ">
+                                <div className="card card-registration my-4 shadow-lg p-3 mb-5 bg-white rounded">
+                                    <div className="row g-0">
+                                        <div className="col-xl-6 d-none d-xl-block">
+                                            <img src={trainpic} alt="train" className="train-img img-fluid"/>
+                                        </div>
+                                        <div className="col-xl-6">
+                                            <Link to="/">
+                                                <button type="button" className="btn-close" aria-label="Close"></button>
+                                            </Link>
+
+                                            <div className="card-body p-md-5 text-black">
+                                                <div className="logo-name shadow p-3 mb-5 bg-white rounded">
+                                                    <p className="sub-logo-name"> RodeSign</p>
+                                                </div>
+
+
+                                                <div className="acc-des text-uppercase text-center mb-5">
+                                                    Let’s Create Your Account
+                                                </div>
+
+
+                                                <div className="row">
+                                                    <div className="col-md-6 mb-4">
+                                                        <div className="form-outline">
+                                                            <label className="form-label" htmlFor="form3Example1m">First
+                                                                name</label>
+                                                            <input type="text" id="form3Example1m"
+                                                                   className="form-control form-control-lg"
+                                                                   placeholder="First name"
+                                                                   onChange={handleFirstNameChange}
+                                                                   required={1}
+                                                            />
+
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-6 mb-4">
+                                                        <div className="form-outline">
+                                                            <label className="form-label" htmlFor="form3Example1n">Last
+                                                                name</label>
+                                                            <input type="text" id="form3Example1n"
+                                                                   className="form-control form-control-lg"
+                                                                   placeholder="Last name"
+                                                                   onChange={handleLastNameChange}
+                                                                   required={1}/>
+
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div className="col-md-6 mb-4">
-                                                    <div className="form-outline">
-                                                        <label className="form-label" htmlFor="form3Example1n">Last
-                                                            name</label>
-                                                        <input type="text" id="form3Example1n"
-                                                               className="form-control form-control-lg" placeholder="Last
-                                                            name"/>
 
+
+                                                <div className="row">
+                                                    <div className="form-outlin mb-4">
+                                                        <div className="form-outline">
+                                                            <label className="reg-ven-label">Phone Number</label>
+                                                            <input type="tel" className="form-control"
+                                                                // country="US"
+                                                                // value={value}
+                                                                // onChange={setValue}
+                                                                   onChange={handleMobileChange}
+                                                                   required={1}
+                                                            />
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
 
+                                                <div className="form-outline mb-4">
+                                                    <label className="form-label" htmlFor="form3Example97">Email
+                                                        ID</label>
+                                                    <input type="text" id="form3Example97"
+                                                           className="form-control form-control-lg"
+                                                           placeholder="Email"
+                                                           onChange={handleEmailChange}
+                                                           required={1}/>
 
-                                            <div className="row">
-                                                <div className="form-outlin mb-4">
-                                                    <div className="form-outline">
-                                                        <label className="reg-ven-label">Phone Number</label>
-                                                        <PhoneInput className="form-control"
-                                                                    country="US"
-                                                                    value={value}
-                                                                    onChange={setValue}/>
-                                                    </div>
                                                 </div>
+
+                                                <div className="form-outline mb-4">
+                                                    <label className="form-label"
+                                                           htmlFor="form3Example4cg">Password</label>
+                                                    <input type="password" id="form3Example4cg"
+                                                           className="form-control form-control-lg"
+                                                           placeholder="Password"
+                                                           onChange={handlePasswordChange}
+                                                           required={1}/>
+
+                                                </div>
+
+
+                                                <div className="d-flex justify-content-end pt-3">
+                                                    {/*<Link className="register-btn" to="/emailVerification">*/}
+                                                        <button type="button" className="btn  btn-lg ms-2"
+                                                                onClick={handleSubmit}>Register
+                                                        </button>
+                                                    {/*</Link>*/}
+                                                </div>
+
+                                                <p className="text-center text-muted mt-5 mb-0">Have already an
+                                                    account?
+                                                    <Link to="/SignIn" className="fw-bold text-body">
+                                                        Login here
+                                                    </Link>
+                                                </p>
+
+
                                             </div>
-
-                                            <div className="form-outline mb-4">
-                                                <label className="form-label" htmlFor="form3Example97">Email
-                                                    ID</label>
-                                                <input type="text" id="form3Example97"
-                                                       className="form-control form-control-lg" placeholder="Email"/>
-
-                                            </div>
-
-                                            <div className="form-outline mb-4">
-                                                <label className="form-label" htmlFor="form3Example4cg">Password</label>
-                                                <input type="password" id="form3Example4cg"
-                                                       className="form-control form-control-lg" placeholder="Password"/>
-
-                                            </div>
-
-
-                                            <div className="d-flex justify-content-end pt-3">
-                                                <Link className="register-btn" to="/emailVerification">
-                                                    <button type="button" className="btn  btn-lg ms-2">Register
-                                                    </button>
-                                                </Link>
-                                            </div>
-
-                                            <p className="text-center text-muted mt-5 mb-0">Have already an
-                                                account?
-                                                <Link to="/SignIn" className="fw-bold text-body">
-                                                    Login here
-                                                </Link>
-                                            </p>
-
-
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
 
 
-        </div>
+            </div>
 
-    );
-}
+        );
+    }
 
-export default CreateAccountCom;
+    export default CreateAccountCom;
